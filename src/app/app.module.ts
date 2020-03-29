3685530fc4b2f6b930a0cf39913c9f5797ce284e
Ray4898 <60079565+Ray4898@users.noreply.github.com>
2020-03-29 12:20:16 -1000

    form controll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OwnerFormComponent } from './owner-form/owner-form.component';
-import { FrontDisplayComponent } from './front-display/front-display.component';
+/*import { FrontDisplayComponent } from './front-display/front-display.component';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BackDisplayComponent } from './back-display/back-display.component';
+import { BackDisplayComponent } from './back-display/back-display.component';*/
 import { MatCheckboxModule} from '@angular/material/checkbox';
 import { KioskIntialComponent } from './kiosk-intial/kiosk-intial.component';
 import { KioskStartComponent } from './kiosk-start/kiosk-start.component';
@@ -21,8 +21,8 @@ import { NotPrequalComponent } from './not-prequal/not-prequal.component';
   declarations: [
     AppComponent,
     OwnerFormComponent,
-    FrontDisplayComponent,
-    BackDisplayComponent,
+    //FrontDisplayComponent,
+    //BackDisplayComponent,
     KioskIntialComponent,
     KioskStartComponent,
     SearchScreenComponent,
@@ -32,10 +32,11 @@ import { NotPrequalComponent } from './not-prequal/not-prequal.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatTableModule,
+   // BrowserAnimationsModule,
+  //  MatTableModule,
     MatCheckboxModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/owner-form/owner-form.component.ts
+++ src/app/owner-form/owner-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Owner } from '../owner';
+import { FormGroup, FormBuilder } from  '@angular/forms';
 
 @Component({
   selector: 'app-owner-form',
@@ -7,13 +8,38 @@ import { Owner } from '../owner';
   styleUrls: ['./owner-form.component.css']
 })
 export class OwnerFormComponent {
+  checkinForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.createContactForm();
+  }
+  createContactForm() {
+    this.checkinForm = this.formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      areaCode1: [''],
+      areaCode2: [''],
+      phone3: [''],
+      phone4: [''],
+      MicoID: [''],
+      email: ['']
+    });
+  }
 
-  names = ['John Doe', 'Jane Doe'];
-  model = new Owner('Doe', 'John', 808, 123, 4567, 'email@email.com', 'A1234B0123');
-  submitted = false;
-  onSubmit() { this.submitted = true; }
+  onSubmit() {
+    const firstname = this.checkinForm.value.firstName;
+    const lastname = this.checkinForm.value.lastName;
+    const phone = this.checkinForm.value.areaCode1 + this.checkinForm.value.areaCode2 + this.checkinForm.value.phone3 + this.checkinForm.value.phone4;
+    const email = this.checkinForm.value.email;
+    const MicroID = this.checkinForm.value.MicoID;
+    console.log('first name : ', firstname);
+    console.log('lastname : ', lastname);
+    console.log('phone : ', phone);
+    console.log('email : ', email);
+    console.log('Microchip Number : ', MicroID);
+  }
+}
 
-  // TODO: Remove this when done
+// TODO: Remove this when done
   // get diagnostic() { return JSON.stringify(this.model); }
 
-}
+
